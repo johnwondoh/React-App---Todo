66294c6668d78c67c1cd7a98fdcaab4fa3b96a31
John Wondoh <johnwondoh@Johns-MacBook-Pro.local>
2019-02-01 16:17:43 +1030

    react todo app initial commit

---
+++ src/components/Todos.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import TodoItem from './TodoItem';
+import PropTypes from 'prop-types'
+
+
+class Todos extends Component {
+  markComplete = () => {
+    console.log('hello');
+  }
+  render() {
+    return (
+        this.props.todos.map((todo) => (
+            // <h3> { todo.title } </h3>
+            <TodoItem key={todo.id} todo={todo } markComplete={this.markComplete}/>
+        ))
+     );
+  }
+}
+
+// Defining proptype
+Todos.propTypes = {
+  todos: PropTypes.array.isRequired
+}
+
+export default Todos;
